2d8bb42db9700d7734da739088dba789e897e74b
Russell Bicknell <bicknellr@fuchsine.local>
2013-08-31 21:27:15 -0500

    IONumber should be the interface for plain numbers.

---
+++ IOInterfaces/IONumber.js
@@ -0,0 +1,43 @@
+module.declare("IOInterfaces/IONumber", [
+  "Graph"
+], function () {
+
+  var Graph = module.require("Graph");
+
+  var IONumber = (function () {
+    function IONumber (params) {
+      params = (typeof params == "undefined" ? {} : params);
+
+      Graph.IOInterface.apply(this);
+
+      this.on_set = params.onSet || function () {};
+    }
+
+    IONumber.prototype = Object.create(Graph.IOInterface.prototype);
+
+    IONumber.prototype.set = function (new_value) {
+      if (typeof new_value !== "number") {
+        console.warn("IONumber(.set): New value is not a number!");
+        return;
+      }
+
+      this.value = new_value;
+
+      this.on_set(this.value);
+
+      var connections = this.getConnections();
+      for (var i = 0; i < connections.length; i++) {
+        connections.set(this.value);
+      }
+    };
+
+    IONumber.prototype.get = function () {
+      return this.value;
+    };
+
+    return IONumber;
+  })();
+
+  return IONumber;
+
+});
