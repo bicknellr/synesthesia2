9014e1c1b76bccde95e1c5fba88a501ebdb40d62
Russell Bicknell <bicknellr@fuchsine.local>
2013-08-31 19:30:53 -0500

    Module system now checks for unused dependencies.
    Note: Nodes, IOInterfaces, and DataTypes are still being directly
    assigned to properties of the Synesthesia object which causes the
    module checker to show them as unused dependencies. These modules
    need to be made side-effect free and requested using the
    module.require method instead of just asking for them in a module
    declaration and taking their results from the places they are
    assigned to on other objects.

--- module.js
+++ module.js
@@ -1,10 +1,14 @@
 window.module = {
   modules: {},
 
+  // Used to check for dependency usage for a given module.
+  _unused_dependencies_stack: [],
+
   check_modules: function () {
     var changes = false;
 
-    to_next_module: for (var mod_ix in this.modules) {
+    to_next_module:
+    for (var mod_ix in this.modules) {
       cur_module = this.modules[mod_ix];
 
       if (cur_module.processed) continue;
@@ -21,33 +25,20 @@ window.module = {
         }
       }
 
-      console.log("module: Processing '" + cur_module.name + "'.");
+      console.log("module: Processing '" + cur_module.name + "'...");
 
+      // Record global variables.
+      var before = [];
+      for (var prop in window) {
+        before.push(prop);
+      }
+
+      // Record module dependencies.
+      this._unused_dependencies_stack.push([].concat(cur_module.dependencies));
+
+      // Run the module.
       try {
-        var before = [];
-        for (var prop in window) {
-          before.push(prop);
-        }
-
-        // Run the module.
         cur_module.exports = cur_module.callback.apply(window);
-
-        var after = [];
-        for (var prop in window) {
-          after.push(prop);
-        }
-
-        var polluted_variables = [];
-
-        for (var i = 0; i < after.length; i++) {
-          if (before.indexOf(after[i]) == -1) {
-            polluted_variables.push(after[i]);
-          }
-        }
-        
-        if (polluted_variables.length > 0) {
-          console.warn("module: Global namespace pollution found in '" + cur_module.name + "':\n    " + (polluted_variables.join("\n    ")));
-        }
       } catch (e) {
         if (e.stack) {
           console.error("module: Processing '" + cur_module.name + "' failed:\nStack:\n" + e.stack.toString() + "\nError:\n" + e.toString());
@@ -57,6 +48,30 @@ window.module = {
         }
         continue to_next_module;
       };
+
+      // Check for new global variables.
+      var after = [];
+      for (var prop in window) {
+        after.push(prop);
+      }
+
+      var polluted_variables = [];
+
+      for (var i = 0; i < after.length; i++) {
+        if (before.indexOf(after[i]) == -1) {
+          polluted_variables.push(after[i]);
+        }
+      }
+      
+      if (polluted_variables.length > 0) {
+        console.warn("module: Global namespace pollution found in '" + cur_module.name + "':\n    " + (polluted_variables.join("\n    ")));
+      }
+
+      // Check for unused dependencies.
+      var last_stack = this._unused_dependencies_stack.pop();
+      if (last_stack.length > 0) {
+        console.warn("module: Module '" + cur_module.name + "' did not require (or has multiple copies of) the following modules in its dependency list:\n    " + (last_stack.join("\n    ")));
+      }
 
       changes = true;
       cur_module.processed = true;
@@ -81,6 +96,14 @@ window.module = {
     if (!this.modules[name]) {
       throw new Error("module: Unsatisfied dependency (" + name + ") requested!");
     }
+
+    // Mark requested module dependencies.
+    var _unused_dependencies_stack = this._unused_dependencies_stack;
+    var last_stack = _unused_dependencies_stack[_unused_dependencies_stack.length - 1];
+    if (last_stack.indexOf(name) != -1) {
+      last_stack.splice(last_stack.indexOf(name), 1);
+    }
+
     return this.modules[name].exports;
   }
 };
