408f9a33964efd9d166101fe3e3df930d366a243
Russell Bicknell <bicknellr@fuchsine.local>
2013-09-04 22:18:15 -0500

    Added MIDILogger node to help with debugging.

---
+++ Nodes/MIDILogger.js
@@ -0,0 +1,33 @@
+module.declare("Nodes/MIDILogger", [
+  "Graph",
+  "IOInterfaces/MIDI"
+], function () {
+
+  var Graph = module.require("Graph");
+
+  var MIDI = module.require("IOInterfaces/MIDI");
+
+  var MIDILogger = (function () {
+    function MIDILogger () {
+      Graph.Node.apply(this);
+
+      this.inputs = {
+        "midi": new MIDI({
+          onMessage: this.onMessage.bind(this)
+        })
+      };
+
+      this.outputs = {};
+    }
+
+    MIDILogger.prototype = Object.create(Graph.Node.prototype);
+
+    MIDILogger.prototype.onMessage = function (message) {
+      console.log(message);
+    };
+
+    return MIDILogger;
+  })();
+
+  return MIDILogger;
+});
